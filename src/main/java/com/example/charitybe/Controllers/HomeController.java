35412f2f815fe624e22ec0e33c0aad3b53369a6a
devthuan <devthuan24@gmail.com>
2025-09-25 22:35:31 +0700

    fix path

--- src/main/java/com/example/charitybe/Controllers/HomeController.java
+++ src/main/java/com/example/charitybe/Controllers/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "Welcome to the Charity API cicd!";
+        return "Welcome to the Charity API ^^!";
     }
 
 }
